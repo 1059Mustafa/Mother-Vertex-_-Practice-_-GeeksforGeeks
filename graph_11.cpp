547e5497230d19bcf411c1082b38d191fcb0057d
1059Mustafa <72117500+1059Mustafa@users.noreply.github.com>
2021-02-16 17:05:38 +0530

    Add files via upload

---
+++ graph_11.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    void dfs(int start,int V, vector<int>g[],vector<bool>&vis)
+    {
+        vis[start]=true;
+        for(auto node:g[start])
+        {
+            if(!vis[node])
+            {
+                dfs(node,start,g,vis);
+            }
+        }
+    }
+
+	int findMotherVertex(int V, vector<int>adj[])
+	{
+	    int last_dfs;
+	    vector<bool>vis(V,false);
+	    for(int i=0;i<V;i++)
+	    {
+	        if(!vis[i])
+	        {
+	            dfs(i,V,adj,vis);
+	            last_dfs=i;
+	        }
+	    }
+	    
+	    for(int i=0;i<V;i++)
+	    {
+	        vis[i]=false;
+	    }
+	    dfs(last_dfs,V,adj,vis);
+	    for(int i=0;i<V;i++)
+	    {
+	        if(vis[i]==false)
+	        {
+	            return -1;
+	        }
+	    }
+	    return last_dfs;
+	}
+
+};
